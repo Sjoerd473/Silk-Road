66c51aeeca927fdfee19d57338b6517c97dce6da
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-09-05 17:05:39 +0200

    added basic function for generating cities

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+let tileNumber = 1
+
+function createCity() {
+    if (tileNumber = 1){
+        //create first city
+        let parent = document.querySelector('.firstRow');
+        console.log(parent)
+        let cityone = document.createElement('div');
+        cityone.classList.add('city', `tile${tileNumber}`)
+        cityone.id = 'test'
+        cityone.textContent = 'CO';
+        parent.appendChild(cityone);
+
+        tileNumber++
+    } else if (tileNumber = 10){
+        //create second city
+        tileNumber++
+    }
+
+}
+
 // For rolling any amount of dice
+
 function rollDice(min, max) {
     return Math.ceil(Math.random() * (max - min) + min);
 }
@@ -14,13 +36,13 @@ function createTileRow() {
     x.insertBefore(row,null);
 }
 
- function createTiles(start,finish){   
+ function createTiles(finish){   
     const parent = document.querySelector('.tileRow')
     
 
-    for (let i = start; i <= finish; i++){
+    for (; tileNumber < finish; tileNumber++){
     let tile = document.createElement('div');
-    tile.classList.add('point',`tile${i}`);
+    tile.classList.add('point',`tile${tileNumber}`);
     console.log(tile)
    
     parent.appendChild(tile);
@@ -29,4 +51,10 @@ function createTileRow() {
 
 function createTileColumn(start, finish) {
 
+}
+
+function create(){
+    createCity()
+    createTileRow()
+    createTiles(10)
 }
