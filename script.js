83fe41d6cb61026e3afe3876747bccaafa7e8aa8
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-09-05 16:42:26 +0200

    added basic function for generating some tiles and a row containing them

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-/*function rollD6(n) {
-    let diceRoll = 0
-    for (let i = 0; i < n; i++){
-  const diceRoll =  Math.floor((Math.random()* 6) + 1)
-  console.log(diceRoll);
-  let total =+ diceRoll
-
-}
-let total =+ diceRoll
-console.log(total);
-}
-*/
-
-function rollD6(min, max){
+// For rolling any amount of dice
+function rollDice(min, max) {
     return Math.ceil(Math.random() * (max - min) + min);
+}
+
+function createTileRow() {
+    let y = document.querySelector('#test');
+    let x = document.querySelector('#test').parentNode;
+    
+    
+    let row = document.createElement('div');
+    row.classList.add('tileRow');
+    
+    x.insertBefore(row,null);
+}
+
+ function createTiles(start,finish){   
+    const parent = document.querySelector('.tileRow')
+    
+
+    for (let i = start; i <= finish; i++){
+    let tile = document.createElement('div');
+    tile.classList.add('point',`tile${i}`);
+    console.log(tile)
+   
+    parent.appendChild(tile);
+    }
+}
+
+function createTileColumn(start, finish) {
+
 }
